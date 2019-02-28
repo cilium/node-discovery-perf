92a3d7cf3bc37cbc426e9d1fbe43c2832b2ec1d2
Maciej Kwiek <maciej@covalent.io>
2019-02-28 14:38:18 +0100

    Script for plotting results
    Signed-off-by: Maciej Kwiek <maciej@covalent.io>

---
+++ plot.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+nodes = []
+init_discovery = []
+additional_discovery = []
+
+with open("results") as file:
+    count = 0
+    for line in file:
+        if count == 0:
+            nodes.append(int(line.split(" ")[0]))
+        if count == 1:
+            init_discovery.append(float(line.split(" ")[3].strip("s,")))
+        if count == 2:
+            additional_discovery.append(float(line.split(" ")[7].strip("s,")))
+        count = (count + 1) % 3
+
+print(nodes)
+print(init_discovery)
+print(additional_discovery)
+
+plt.plot(nodes, init_discovery)
+plt.ylabel("seconds")
+plt.xlabel("nodes")
+plt.savefig("init.svg")
+plt.close()
+
+plt.plot(nodes, additional_discovery)
+plt.ylabel("seconds")
+plt.xlabel("nodes")
+plt.savefig("additional.svg")
